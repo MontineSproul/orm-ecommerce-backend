0b126944d01d1d1e0dc26fa73ff1d735f326c7fd
Ashley Montine Sproul <montinesproul@gmail.com>
2023-04-03 10:15:40 -0400

    updated routes

--- models/index.js
+++ models/index.js
@@ -1,24 +1,23 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsto(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 // Categories have many Products
-Category.hasmany(Product, {
+Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongstomany(Tag, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
 })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
 })
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,89 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll ({
+    include: [
+      {
+        model: Product,
+        attributes: ["product_name"],
+      },
+    ],
+  })
+    .then ((dataDb) => res.json(dataDb))
+    .catch((err)=> {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["product_name"],
+      }
+    ],
+  })
+    .then(dataDb => {
+      if(!dataDb){
+        res.status(404).json({ message: "User not found"})
+        return;
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+  .then((dataDb) => res.json(dataDb))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => {
+      res.status(200).json(category);
+    }).catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where:{
+      id: req.params.id,
+    },
+  })
+    .then(dataDb => {
+      if(!dataDb){
+        res.status(404).json({ message: "User not found"})
+        return;
+      }
+      res.status(200).json(dataDb);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
